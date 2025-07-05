4e160fce3334c2875ae6b3ba97959a10a589249a
Stn77 <fudinyusuf20@gmail.com>
2025-07-05 19:07:52 +0700

    Add Api setup

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
+<p align="center">
+<a href="https://medium.com/@1415sandalanka/laravel-11-rest-api-crud-with-best-practices-fcc26505e0d2">Source code</a>
+</p>
+
 ## About Laravel
 
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
---
+++ app/Repositories/ProdukRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\Interfaces\ProdukRepositoryInterface;
+use App\Models\Produk;
+
+class ProdukRepository implements ProdukRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function index(){
+        return Produk::all();
+    }
+
+    public function getById($id){
+       return Produk::findOrFail($id);
+    }
+
+    public function store(array $data){
+       return Produk::create($data);
+    }
+
+    public function update(array $data,$id){
+       return Produk::whereId($id)->update($data);
+    }
+    
+    public function delete($id){
+       Produk::destroy($id);
+    }
+}
